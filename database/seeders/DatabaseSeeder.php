c8da51936647aed750f84a63d02de4a14ed8cd84
MichelCode <michelseocode.com>
2024-01-18 14:08:35 +0100

    Seeder en fonction

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\Company;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -22,5 +21,6 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call(CompanySeeder::class);
+        $this->call(VulnerabilitySeeder::class);
     }
 }
