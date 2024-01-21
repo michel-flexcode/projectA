b7997a8b994e62522e4517db4712d55f16729b4e
MichelCode <michelseocode.com>
2024-01-21 20:42:38 +0100

    Login page on work

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -1,13 +1,11 @@
 <x-guest-layout>
 
-    <div class="flex items-center justify-center h-screen bg-gradient-to-b from-blue-500 to-purple-700">
+    <div class="flex items-center justify-center h-screen"
+        style="background-image: url('{{ asset('storage/backgrounds/HopliteV2.png') }}'); background-size: cover;">
+
         <div class="text-center text-white">
             <h1 class="text-4xl font-bold mb-4">Bienvenue sur Instameme !</h1>
             <p class="text-lg mb-8">Découvrez et partagez des mèmes hilarants.</p>
-
-            {{-- Remplacez le chemin de l'image par votre image --}}
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO9bQ9-vHeqm-_eAsWgiwO8msa37ZC9B17xVGPH8oPiQ&s"
-                alt="Description de l'image" class="w-full max-w-md mx-auto rounded-md shadow-lg">
 
             {{-- Formulaire de connexion --}}
             <div class="mt-8">
@@ -16,5 +14,16 @@
             </div>
         </div>
     </div>
+
+    {{-- <div class="flex items-center space-x-4">
+
+        <a href="{{ route('login') }}" class="font-medium text-gray-600 hover:text-gray-500 transition">Login</a>
+        <a href="{{ route('register') }}" class="font-medium text-gray-600 hover:text-gray-500 transition">Register</a>
+    </div> --}}
+
+    {{-- <img url="{{ asset('storage/app/public/backgrounds/HopliteV2.jpg') }}">
+    aa
+    <img src="{{ asset('storage/backgrounds/HopliteV2.png') }}" alt="Description de l'image">
+    aaa --}}
 
 </x-guest-layout>
