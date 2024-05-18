d76769abd52e462bd10cbde8833fa8232bab258f
MichelCode <michelseocode.com>
2024-05-18 20:59:59 +0200

    Suite 18/05/2024

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,9 @@
-<x-app-layout>
+{{-- <x-app-layout>
     <div class="flex flex-col justify-center h-screen"
         style="background-image: url('{{ asset('storage/backgrounds/HopliteV2.png') }}'); background-size: cover;">
+</x-app-layout> --}}
+
+<x-app-layout>
+    <div class="flex flex-col justify-center h-screen" style="background-color: #1A1C24;">
+    </div>
 </x-app-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,25 +1,26 @@
 <!-- resources/views/layouts/navigation.blade.php -->
-<nav x-data="{ open: false }" class="bg-gray-800 border-b border-gray-100"
-    style="background-image: url('{{ asset('storage/backgrounds/EYlogo.png') }}'); background-size: cover;">
+<nav x-data="{ open: false }" class="border-b border-gray-100" style="background-color: #1A1C24;">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    {{-- <div class="max-w-7xl mx-auto px-2 sm:px-4 lg:px-6"> --}}
+    <div class="max-w-7xl mx-auto px-2 sm:px-2 lg:px-2">
+
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                <div class="shrink-0 flex items-center h-full">
+                    <a href="{{ route('dashboard') }}" class="h-full">
+                        <x-application-logo class="block h-full w-auto fill-current text-gray-600" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <!-- Search Bar -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex items-center">
+                <div class="hidden space-x-4 sm:-my-px sm:ml-2 sm:flex items-center">
                     <form method="GET" action="{{ route('search') }}" class="flex">
-                        <input type="text" name="query" placeholder="Search..."
-                            class="px-4 py-2 border rounded-l-md focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent">
+                        <input type="text" name="query" placeholder="Search Reports:"
+                            class="px-2 py-1 border rounded-l-md focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent">
                         <button type="submit"
-                            class="px-4 py-2 bg-indigo-600 text-white rounded-r-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2">
+                            class="px-2 py-1 bg-indigo-600 text-white rounded-r-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2">
                             Search
                         </button>
                     </form>
@@ -28,14 +29,14 @@
             </div>
 
             <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
+            <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button
-                            class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
 
-                            <div class="ml-1">
+                            <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
                                     viewBox="0 0 20 20">
                                     <path fill-rule="evenodd"
@@ -47,6 +48,10 @@
                     </x-slot>
 
                     <x-slot name="content">
+                        <x-dropdown-link :href="route('profile.edit')">
+                            {{ __('Profile') }}
+                        </x-dropdown-link>
+
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
