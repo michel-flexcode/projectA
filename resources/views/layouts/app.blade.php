3be1460c538146918ee282e55f06a1b4e86e10fa
MichelCode <michelseocode.com>
2024-05-20 21:50:14 +0200

    Sidebar Prototype

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,22 +17,28 @@
 </head>
 
 <body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100">
-        @include('layouts.navigation')
+    <div class="min-h-screen bg-gray-100 flex">
+        <!-- Sidebar -->
+        @include('layouts.sidebar')
 
-        <!-- Page Heading -->
-        @if (isset($header))
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-        @endif
+        <!-- Main Content -->
+        <div class="flex-1 flex flex-col">
+            @include('layouts.navigation')
 
-        <!-- Page Content -->
-        <main>
-            {{ $slot }}
-        </main>
+            <!-- Page Heading -->
+            @if (isset($header))
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endif
+
+            <!-- Page Content -->
+            <main class="flex-1">
+                {{ $slot }}
+            </main>
+        </div>
     </div>
 </body>
 
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,9 @@
+<!-- resources/views/partials/sidebar.blade.php -->
+<div class="w-64 bg-gray-800 text-white p-4">
+    <h2 class="text-xl font-bold mb-4">Navigation</h2>
+    <ul>
+        <li><a href="#home" class="block py-2">Accueil</a></li>
+        <li><a href="#services" class="block py-2">Services</a></li>
+        <li><a href="#contact" class="block py-2">Contact</a></li>
+    </ul>
+</div>
