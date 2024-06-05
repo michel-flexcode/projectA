34806cb80ed016b39111e66a2c62bcdf299fe648
MichelCode <michelseocode.com>
2024-06-05 18:38:41 +0200

    Pagination

--- resources/views/sidebarpages/consultants.blade.php
+++ resources/views/sidebarpages/consultants.blade.php
@@ -30,6 +30,7 @@
                     Dashboard</a>
             </div>
 
+            <!-- Affichage des consultants -->
             <div class="container mx-auto">
                 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                     @foreach ($consultants as $consultant)
@@ -58,6 +59,8 @@
                 </div>
             </div>
 
+            <!-- Pagination -->
+            {{ $consultants->links() }}
 
         </div>
     </div>
